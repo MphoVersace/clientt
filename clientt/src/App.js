41cf584c6caa81698fa0046ee438713cc335abd3
Mpho Mtlomelo <164197944+MphoVersace@users.noreply.github.com>
2024-08-01 00:57:32 +0200

    react router dom

--- clientt/src/App.js
+++ clientt/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar";
+import HomePage from "./components/HomePage";
+import NewPostForm from "./components/NewPostForm";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavigationBar />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/new" component={NewPostForm} />
+    </Router>
   );
-}
+};
 
 export default App;
