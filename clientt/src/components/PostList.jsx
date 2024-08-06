0d7127f09198a8589eb7c98e51744679adc7d4ee
Mpho Mtlomelo <164197944+MphoVersace@users.noreply.github.com>
2024-08-06 16:27:50 +0200

    images added, styling added

--- clientt/src/components/PostList.jsx
+++ clientt/src/components/PostList.jsx
@@ -2,6 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+import Image from "./assets/pic1.webp";
+import Image2 from "./assets/Pic2.webp";
+import Image3 from "./assets/pic3.webp";
+import Image4 from "./assets/Pic4.webp";
+
+const imageMap = {
+  pic1: Image,
+  Pic2: Image2,
+  pic3: Image3,
+  Pic4: Image4,
+};
+
 const PostList = () => {
   const [posts, setPosts] = useState([]);
 
@@ -23,7 +35,9 @@ const PostList = () => {
       {posts.map((post) => (
         <div key={post.id} className="post">
           <h3>{post.title}</h3>
-          <img src={post.imageUrl} alt={post.title} />
+          <div className="post-image">
+            <img src={imageMap[post.imageUrl] || Image} alt={post.title} />
+          </div>
           <p>{post.content.substring(0, 100)}...</p>
           <Link to={`/post/${post.id}`}>Read more</Link>
         </div>
